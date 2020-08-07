a7f1f58ec13cc1e7989d410a670e2efc6fdc4708
João Pedro De Munno Durante <jaodurante@gmail.com>
2020-08-07 20:00:57 -0300

    edges implemented and bug fixes

--- helpers/DataHelper.py
+++ helpers/DataHelper.py
@@ -1,48 +1,8 @@
 import json
 
 class DataHelper:
-    def getUsers(self):
-        with open('./data/users.json', 'r') as json_file:
-            try:
-                data = json_file.read()
-                return json.loads(data)['data']
-            except Exception as e:
-                print(e)
-
-    def getBooks(self):
-        with open('./data/books.json', 'r') as json_file:
-            try:
-                data = json_file.read()
-                return json.loads(data)['data']
-            except Exception as e:
-                print(e)
-
-    def getGenres(self):
-        with open('./data/genres.json', 'r') as json_file:
-            try:
-                data = json_file.read()
-                return json.loads(data)['data']
-            except Exception as e:
-                print(e)
-
-    def getPurchased(self):
-        with open('./data/purchased.json', 'r') as json_file:
-            try:
-                data = json_file.read()
-                return json.loads(data)['data']
-            except Exception as e:
-                print(e)
-
-    def getViews(self):
-        with open('./data/view.json', 'r') as json_file:
-            try:
-                data = json_file.read()
-                return json.loads(data)['data']
-            except Exception as e:
-                print(e)
-    
-    def getBelongs(self):
-        with open('./data/belongs.json', 'r') as json_file:
+    def getData(self, file):
+        with open('./data/{}.json'.format(file), 'r') as json_file:
             try:
                 data = json_file.read()
                 return json.loads(data)['data']
@@ -51,5 +11,5 @@ class DataHelper:
 
     def getNodeById(self, data, id):
         for item in data:
-            if item.id == id:
+            if item.getProperty('id') == id:
                 return item
--- main.py
+++ main.py
@@ -6,56 +6,42 @@ from helpers.DataHelper import DataHelper
 import json
 
 dataHelper = DataHelper()
-users = dataHelper.getUsers()
-books = dataHelper.getBooks()
-genres = dataHelper.getGenres()
-view = dataHelper.getViews()
-purchaseds = dataHelper.getPurchased()
-belongs = dataHelper.getBelongs()
+users = dataHelper.getData('users')
+books = dataHelper.getData('books')
+genres = dataHelper.getData('genres')
+views = dataHelper.getData('view')
+purchaseds = dataHelper.getData('purchased')
+belongs = dataHelper.getData('belongs')
+interesteds = dataHelper.getData('interested')
 
-### NODES ##
+### NODES ###
 # Persons #
-# joao = Node('Person', {'name': 'Joao'})
-# carol = Node('Person', {'name': 'Caroline'})
-# gui = Node('Person', {'name': 'Guilherme'})
 usersNodes = [ Node('user', user) for user in users ]
 
 # Books
-# hp = Node('Book', {'name': 'Harry Potter'})
-# percy = Node('Book', {'name': 'Percy Jackson'})
-# cityOfPaper = Node('Book', {'name': 'City of Paper'})
-# whoAreYouAlaska = Node('Book', {'name': 'Who are you Alaska?'})
-# brasCubas = Node('Book', {'name': 'Bras Cubas'})
 booksNodes = [ Node('book', book) for book in books ]
 
 # Genres
-# scifi = Node('Genre', {'name': 'sci-fi'})
-# romance = Node('Genre', {'name': 'romance'})
 genresNodes = [ Node('genre', genre) for genre in genres ]
+
 
 ### EDGES ###
 # Purchased
-# purchased = []
-# purchased.append(Edge('purchased', []).link(joao, hp, False))
-# purchased.append(Edge('purchased', []).link(joao, percy, False))
-# purchased.append(Edge('purchased', []).link(carol, cityOfPaper, False))
-# purchased.append(Edge('purchased', []).link(carol, whoAreYouAlaska, False))
 purchasedEdges = [ Edge('purchased', []).link(
-    dataHelper.getNodeById(purchaseds, purchased.userId), dataHelper.getNodeById(purchaseds, purchased.bookId), False
+    dataHelper.getNodeById(usersNodes, purchased['userId']), dataHelper.getNodeById(booksNodes, purchased['bookId']), False
 ) for purchased in purchaseds ]
 
 # Interested
-# interested = []
-# interested.append(Edge('interested', []).link(joao, scifi, False))
-# interested.append(Edge('interested', []).link(carol, scifi, False))
-# interested.append(Edge('interested', []).link(carol, romance, False))
-# interested.append(Edge('interested', []).link(gui, romance, False))
-
+interestedEdges = [ Edge('interested', []).link(
+    dataHelper.getNodeById(usersNodes, interested['userId']), dataHelper.getNodeById(genresNodes, interested['genreId']), False
+) for interested in interesteds ]
 
 # Belongs to the genre
-# belongs = []
-# belongs.append(Edge('belongs', []).link(hp, scifi, False))
-# belongs.append(Edge('belongs', []).link(percy, scifi, False))
-# belongs.append(Edge('belongs', []).link(cityOfPaper, romance, False))
-# belongs.append(Edge('belongs', []).link(whoAreYouAlaska, romance, False))
-# belongs.append(Edge('belongs', []).link(brasCubas, romance, False))
+belongsEdges = [ Edge('belongs', []).link(
+    dataHelper.getNodeById(booksNodes, belong['bookId']), dataHelper.getNodeById(genresNodes, belong['genreId']), False
+) for belong in belongs ]
+
+# View
+viewEdges = [ Edge('view', []).link(
+    dataHelper.getNodeById(usersNodes, view['userId']), dataHelper.getNodeById(booksNodes, view['bookId']), False
+) for view in views ]
